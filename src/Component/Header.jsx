f82d17535a92315036af41a3ea2378df7a5e4a67
Ritiksingh101 <rs1982thakur@gmail.com>
2024-12-22 13:28:40 +0530

    sencd change

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 
 function Header() {
+
+
+   const [show, setShow] =useState(false)
+
   return (
     <div className='header bg-slate-100'>
     <div className='head1'>
@@ -17,7 +21,38 @@ function Header() {
        <label className='home'>
        Contact</label>
        
-    </div>
+    </div> 
+
+   {/*------------------------------------  responsive nav bar degine ------------------------------- */}
+
+    <div  onClick={()=>setShow(true)} className="flex lg:hidden  text-[30px]">
+  <i class="fa fa-bars" aria-hidden="true"></i> 
+  </div>
+
+  <>
+
+{show === true ?
+
+<div className='w-[100%] h-[100vh] fixed top-0 left-0 z-10'>
+          <div onClick={()=> setShow(false)} className='bg-black w-full h-[100vh] opacity-60 fixed cursor-pointer left-0 top-0 ' >
+          </div>
+          <div className='bg-white sidebar-popup lg:h-[100vh] h-[60vh] lg:w-[30%] w-[100%] absolute right-0 z-[11]' >
+            <button className='w-[100%] h-[10vh]  text-[35px] text-start pl-10' onClick={()=> setShow(false)}  >X</button>
+            <div className='w-[90%] h-[25vh] flex gap-[40%] pl-10  '>
+              <div className='flex flex-col gap-[8%]   '>
+                <h4>Language</h4>
+                <label>English</label>
+                <label>French</label>
+                <label>Arabric</label>
+                
+              
+              </div>
+             
+            </div>
+          </div></div> 
+:null}
+
+          </> 
 
    </div>
   )
--- src/Component/Top.jsx
+++ src/Component/Top.jsx
@@ -5,17 +5,17 @@ function Top() {
   return (
     <div>
       <div className='top'>
-        <div className='left'>
+        <div className='left '>
           <label> <i class="fa fa-phone" aria-hidden="true"></i>
-            +00 1234 567</label>
+            +91 6232610873</label>
           <label> <i class="fa fa-telegram" aria-hidden="true"></i>
-            youremail@email.com</label>
+            ritiksingh.dx@email.com</label>
         </div>
         <div className='right'>
           <label><i class="fa fa-facebook top-img1" aria-hidden="true"></i></label>
           <label><i class="fa fa-twitter top-img2" aria-hidden="true"></i></label>
           <label><i class="fa fa-instagram top-img3" aria-hidden="true"></i></label>
-          <label><i class="fa fa-futbol-o top-img4" aria-hidden="true"></i></label>
+          
         </div>
 
       </div>
